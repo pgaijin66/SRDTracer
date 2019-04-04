7560a326b5f85ad6e99ff36a109273e142730875
Prabesh Thapa <pgaijin66@gmail.com>
2019-04-04 14:50:25 +1100

    Update test.py

--- test.py
+++ test.py
@@ -26,4 +26,4 @@ for i in range(line_count):
 pp('Number of packets sent: {}'.format(send_count))
 pp('Number of packet received: {}'.format(received_count))
 pp('Number of packet dropped: {}'.format(dropped_count))
-pp('Simulation tine: {} ms'.format((list_trace[-1].split(' ')[1])))
+pp('Simulation time: {} ms'.format((list_trace[-1].split(' ')[1])))
